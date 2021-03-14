69612cfcb8f36ba190c5c639c42b3c75738115b6
Qianhao Chen <32904769+qianhaochen@users.noreply.github.com>
2021-03-14 18:10:45 +0000

    Loaded csv to database. Implemented basic browse, sort and search functionalities. Reconstructed code to reuse php functions for easier maintainence.

--- www/index.php
+++ www/index.php
@@ -3,18 +3,23 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LAMP STACK</title>
+        <title>Movie Database</title>
+
         <link rel="stylesheet" href="/assets/css/bulma.min.css">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
     </head>
     <body>
         <section class="hero is-medium is-info is-bold">
             <div class="hero-body">
                 <div class="container has-text-centered">
                     <h1 class="title">
-                        LAMP STACK
+                    Movie Database
                     </h1>
                     <h2 class="subtitle">
-                        Your local development environment
+                        Browse all kinds of movies
                     </h2>
                 </div>
             </div>
@@ -44,6 +49,9 @@
                                     mysqli_close($link);
                                     ?>
                                 </li>
+                                <li>Bootstrap 4.6.0</li>
+                                <li><a href="/phpinfo.php">phpinfo()</a></li>
+                                <li><a href="http://localhost:<? print $_ENV['PMA_PORT']; ?>">phpMyAdmin</a></li>
                             </ul>
                         </div>
                     </div>
@@ -52,10 +60,11 @@
                         <hr>
                         <div class="content">
                             <ul>
-                                <li><a href="/phpinfo.php">phpinfo()</a></li>
-                                <li><a href="http://localhost:<? print $_ENV['PMA_PORT']; ?>">phpMyAdmin</a></li>
-                                <li><a href="/test_db.php">Test DB Connection with mysqli</a></li>
-                                <li><a href="/test_db_pdo.php">Test DB Connection with PDO</a></li>
+                                <li><a href="/browse_movies.php">Browse all movies</a></li>
+                                <li><a href="/search.php">Search movies</a></li>
+                                <li><a href="/popular.php">Most Popular</a></li>
+                                <li><a href="/ratings.php">Top Rated</a></li>
+                                <li><a href="/main.php">Main page</a></li>
                             </ul>
                         </div>
                     </div>
