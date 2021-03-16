162c1a4cb854e8bcd406ec2f5c574b9fa62c4088
Qianhao Chen <32904769+qianhaochen@users.noreply.github.com>
2021-03-16 14:59:30 +0000

    Added more views

--- www/home.php
+++ www/home.php
@@ -12,13 +12,18 @@
   <div class="container">
   <?php echo "<h1>All movies</h1>"; ?>
 
-  <?php
-    $sql_query = 'SELECT mov_id, AVG(rating)
-    FROM ratings
-    GROUP BY mov_id
-    ORDER BY AVG(rating) DESC';
-    $col_arr = array('ID', 'Movie Title', 'Genres');
-    display_sql(search_sql($sql_query), $col_arr);
-  ?>
+  <?php 
+    $sql_query = 'SELECT mov_title, gen_name, ave_rating, rating_count
+    FROM movies,
+       (SELECT mov_id,
+        ROUND(AVG(rating),1) AS ave_rating, 
+           COUNT(user_id) AS rating_count 
+        FROM ratings 
+        GROUP BY mov_id 
+       ) AS avgratingbymovies
+    WHERE movies.mov_id = avgratingbymovies.mov_id';
+    $col_arr = array('Title', 'genres', 'Ratings', 'Views');
+    display_sql($sql_query, $col_arr);
+  ?>  
   </div>
 </body>
--- www/top.php
+++ www/top.php
@@ -13,7 +13,7 @@
   <?php echo "<h1>Top Rated</h1>"; ?>
 
   <?php 
-    $sql_query = 'SELECT movies.mov_id, mov_title,rating_count, ave_rating
+    $sql_query = 'SELECT mov_title, ave_rating, rating_count
     FROM movies,
        (SELECT mov_id,
         ROUND(AVG(rating),1) AS ave_rating, 
@@ -23,7 +23,7 @@
        ) AS avgratingbymovies
     WHERE movies.mov_id = avgratingbymovies.mov_id
     ORDER BY ave_rating DESC, rating_count DESC';
-    $col_arr = array('ID', 'Title','Rating Count','Average Ratings');
+    $col_arr = array('Title', 'Ratings', 'Views');
     display_sql($sql_query, $col_arr);
   ?>  
   </div>
