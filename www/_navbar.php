81262e9755e9bcf6bfb19a48cf69ab353d5eb821
nanxiz <nanxilzhang@gmail.com>
2021-03-20 06:41:23 +0000

    add two layer nav bar

--- www/_navbar.php
+++ www/_navbar.php
@@ -5,7 +5,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
-
+  <link href="assets/css/offcanvas.css" rel="stylesheet">
   <style type="text/css">
   @media (prefers-color-scheme: dark) {
     body {
@@ -30,45 +30,42 @@
 include('_search.php');
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark" aria-label="Main navigation">
   <div class="container-fluid">
     <a class="navbar-brand" href="/index.php">
       <i class="bi bi-film"></i>
       Movie Database
     </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler p-0 border-0" type="button" data-bs-toggle="offcanvas" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <div class="navbar-nav">
 
-        <a class="nav-link active" aria-current="page" href="/home.php">Home</a>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
+    <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/home.php">Movielens</a>
         </li>
-
-        <a class="nav-link" href="/popular.php">Most Popular</a>
-        <a class="nav-link" href="/top.php">Top Rated</a>
-        <a class="nav-link" href="/polarising.php">Most Polarising</a>
-        <button onclick="darkmode()" type="button" class="btn btn-outline-dark" data-bs-toggle="button" autocomplete="off" aria-pressed="true"><i class="bi bi-moon-stars"></i></button>
-
-      </div>
-    </div>
+        <li class="nav-item">
+          <a class="nav-link" href="/popular.php">Soon-to-be-released</a>
+        </li>
         
+      </ul>
+
       
-      
-    <form action="home.php" method="GET" class="d-flex">
+    </div>
+  </div>
+</nav>
+
+<div class="nav-scroller bg--body shadow-sm">
+  <nav class="nav nav-underline" aria-label="Secondary navigation">
+
+    <a class="nav-link active" aria-current="page" href="/home.php">All</a>
+    <a class="nav-link" href="/popular.php">Most Popular</a>
+    <a class="nav-link" href="/top.php">Top Rated</a>
+    <a class="nav-link" href="/polarising.php">Most Polarising</a>
+    <form action="home.php" method="GET" class="d-flex justify-content-end" style=' margin-left: 135px; margin-top: 4px; margin-right: 10px; width:1000px'>
       <div class="input-group">
-        <input type="search" name="query" class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search..." aria-label="Search">
+        <input type="search" name="query" class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Search for movies, genres..." aria-label="Search">
           <datalist id="datalistOptions">
             <option value="Forrest Gump">
             <option value="Shawshank Redemption">
@@ -93,8 +90,10 @@ include('_search.php');
       </div>
     </form>
 
-  </div>
-</nav>
+  </nav>
+</div>
+
+
 
 <script>
 function darkmode() {
