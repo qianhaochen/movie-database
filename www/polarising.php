f82ffd1f0118b60f782058710126829d618f9025
nanxiz <nanxilzhang@gmail.com>
2021-03-23 01:23:57 +0000

    debug-polarising

--- www/polarising.php
+++ www/polarising.php
@@ -20,20 +20,17 @@
     </div>
     <div class="my-3 p-3 bg-body rounded shadow-sm">
       <?php
-    $sql_query = 'SELECT movies.mov_id, mov_title,ave_rating ,rating_count, rating_dif
-    FROM movies,
-      (SELECT mov_id,
-        ROUND(AVG(rating),1) AS ave_rating, 
-          COUNT(user_id) AS rating_count 
-        FROM ratings 
-        GROUP BY mov_id 
-      ) AS avgratingbymovies,
-      (SELECT MAX(rating) - MIN(rating) AS rating_dif, 
-        
-      mov_id FROM ratings GROUP BY mov_id) AS ratingPolarise
-    WHERE movies.mov_id = avgratingbymovies.mov_id AND movies.mov_id = ratingPolarise.mov_id
-    ORDER BY rating_dif DESC, rating_count DESC';
-    $col_arr = array('Title','Ratings','Views','Rating Difference');
+    $sql_query = 'SELECT movies.mov_id, movies.mov_title,ave_rating , rating_dif, ROUND(CVrating,3) FROM movies,
+    (SELECT mov_id,ROUND(AVG(rating),1) AS ave_rating, COUNT(user_id) AS rating_count 
+    FROM ratings GROUP BY mov_id ) AS avgratingbymovies,
+    (SELECT ratings.mov_id,mov_title,MAX(rating) - MIN(rating) AS rating_dif,
+     STDDEV(1.0*rating) / AVG(1.0*rating) AS CVrating
+    FROM ratings,movies
+    WHERE ratings.mov_id = movies.mov_id
+    GROUP BY ratings.mov_id) AS ratingPolarise WHERE 
+    movies.mov_id = avgratingbymovies.mov_id AND 
+    movies.mov_id = ratingPolarise.mov_id ORDER BY rating_dif DESC, CVrating DESC';
+    $col_arr = array('Title','Average Rating','Rating Difference','Coefficient of Variation');
     display_sql($sql_query, $col_arr);
   ?>
     </div>
